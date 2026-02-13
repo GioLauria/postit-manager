1ea727f893a9c09ec4be0ad85f8dd8d7477b4556
Giovanni Lauria <giovanni.lauria@gmail.com>
2026-02-13 14:01:25 +0100

    Improve GUI look and feel: add colors, fonts, centering, better layout

--- gui.c
+++ gui.c
@@ -1,21 +1,50 @@
 #include "postit.h"
 
 HWND hList, hTitle, hContent;
+HFONT hFont;
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
-        case WM_CREATE:
-            hList = CreateWindow("LISTBOX", NULL, WS_CHILD | WS_VISIBLE | WS_VSCROLL | LBS_NOTIFY, 10, 10, 380, 200, hwnd, (HMENU)IDC_LIST, NULL, NULL);
-            CreateWindow("STATIC", "Titolo:", WS_CHILD | WS_VISIBLE, 10, 220, 50, 20, hwnd, NULL, NULL, NULL);
-            hTitle = CreateWindow("EDIT", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER, 70, 220, 320, 20, hwnd, (HMENU)IDC_TITLE, NULL, NULL);
-            CreateWindow("STATIC", "Contenuto:", WS_CHILD | WS_VISIBLE, 10, 250, 70, 20, hwnd, NULL, NULL, NULL);
-            hContent = CreateWindow("EDIT", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER | ES_MULTILINE | WS_VSCROLL, 70, 250, 320, 60, hwnd, (HMENU)IDC_CONTENT, NULL, NULL);
-            CreateWindow("BUTTON", "Aggiungi", WS_CHILD | WS_VISIBLE, 10, 320, 80, 30, hwnd, (HMENU)IDC_ADD, NULL, NULL);
-            CreateWindow("BUTTON", "Modifica", WS_CHILD | WS_VISIBLE, 100, 320, 80, 30, hwnd, (HMENU)IDC_EDIT, NULL, NULL);
-            CreateWindow("BUTTON", "Elimina", WS_CHILD | WS_VISIBLE, 190, 320, 80, 30, hwnd, (HMENU)IDC_DELETE, NULL, NULL);
-            CreateWindow("BUTTON", "Salva ed Esci", WS_CHILD | WS_VISIBLE, 280, 320, 110, 30, hwnd, (HMENU)IDC_SAVE, NULL, NULL);
+        case WM_CREATE: {
+            hFont = CreateFont(16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Segoe UI");
+            
+            hList = CreateWindow("LISTBOX", NULL, WS_CHILD | WS_VISIBLE | WS_VSCROLL | LBS_NOTIFY, 20, 20, 360, 180, hwnd, (HMENU)IDC_LIST, NULL, NULL);
+            SendMessage(hList, WM_SETFONT, (WPARAM)hFont, TRUE);
+            
+            CreateWindow("STATIC", "Titolo:", WS_CHILD | WS_VISIBLE, 20, 210, 60, 25, hwnd, NULL, NULL, NULL);
+            hTitle = CreateWindow("EDIT", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL, 90, 210, 290, 25, hwnd, (HMENU)IDC_TITLE, NULL, NULL);
+            SendMessage(hTitle, WM_SETFONT, (WPARAM)hFont, TRUE);
+            
+            CreateWindow("STATIC", "Contenuto:", WS_CHILD | WS_VISIBLE, 20, 245, 70, 25, hwnd, NULL, NULL, NULL);
+            hContent = CreateWindow("EDIT", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER | ES_MULTILINE | WS_VSCROLL | ES_AUTOVSCROLL, 90, 245, 290, 60, hwnd, (HMENU)IDC_CONTENT, NULL, NULL);
+            SendMessage(hContent, WM_SETFONT, (WPARAM)hFont, TRUE);
+            
+            CreateWindow("BUTTON", "Aggiungi", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 20, 320, 80, 30, hwnd, (HMENU)IDC_ADD, NULL, NULL);
+            CreateWindow("BUTTON", "Modifica", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 110, 320, 80, 30, hwnd, (HMENU)IDC_EDIT, NULL, NULL);
+            CreateWindow("BUTTON", "Elimina", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 200, 320, 80, 30, hwnd, (HMENU)IDC_DELETE, NULL, NULL);
+            CreateWindow("BUTTON", "Salva ed Esci", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 290, 320, 100, 30, hwnd, (HMENU)IDC_SAVE, NULL, NULL);
+            
+            // Set fonts for buttons and statics
+            EnumChildWindows(hwnd, (WNDENUMPROC)SetFontProc, (LPARAM)hFont);
+            
             update_list(hList);
             break;
+        }
+        case WM_CTLCOLORSTATIC: {
+            HDC hdc = (HDC)wParam;
+            SetBkColor(hdc, RGB(255, 255, 224)); // Light yellow background
+            return (LRESULT)CreateSolidBrush(RGB(255, 255, 224));
+        }
+        case WM_CTLCOLOREDIT: {
+            HDC hdc = (HDC)wParam;
+            SetBkColor(hdc, RGB(255, 255, 255)); // White for edits
+            return (LRESULT)CreateSolidBrush(RGB(255, 255, 255));
+        }
+        case WM_CTLCOLORLISTBOX: {
+            HDC hdc = (HDC)wParam;
+            SetBkColor(hdc, RGB(255, 255, 255));
+            return (LRESULT)CreateSolidBrush(RGB(255, 255, 255));
+        }
         case WM_COMMAND:
             switch (LOWORD(wParam)) {
                 case IDC_ADD:
@@ -46,10 +75,16 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             }
             break;
         case WM_DESTROY:
+            DeleteObject(hFont);
             PostQuitMessage(0);
             break;
         default:
             return DefWindowProc(hwnd, msg, wParam, lParam);
     }
     return 0;
+}
+
+BOOL CALLBACK SetFontProc(HWND hwnd, LPARAM lParam) {
+    SendMessage(hwnd, WM_SETFONT, lParam, TRUE);
+    return TRUE;
 }
